c14d54e6031f6dc924d6d921bf1b818aa507a63f
Mayel de Borniol <git@mayel.space>
2022-12-03 09:31:56 +1300

    Fix error handling

--- lib/mailer.ex
+++ lib/mailer.ex
@@ -1,13 +1,17 @@
 defmodule Bonfire.Mailer do
+  alias Bonfire.Common.Utils
   alias Bonfire.Common.Config
   use Bamboo.Mailer, otp_app: Config.get!(:otp_app)
   alias Bamboo.Email
   import Untangle
 
+  @default_email "noreply@bonfire.local"
+  @team_email "team@bonfire.cafe"
+
   def send_now(email, to, opts \\ []) do
     config = Config.get(__MODULE__, [])
 
-    from = opts[:from] || Keyword.get(config, :reply_to, "noreply@bonfire.local")
+    from = opts[:from] || Keyword.get(config, :reply_to, @default_email)
 
     try do
       mail =
@@ -15,9 +19,13 @@ defmodule Bonfire.Mailer do
         |> Email.from(from)
         |> Email.to(to)
 
-      deliver_now(mail)
+      with {:ok, _done} <- deliver_now(mail) do
+        {:ok, mail}
+      else
+        {:error, e} -> handle_error(e)
+        other -> handle_error(other)
+      end
 
-      {:ok, mail}
     rescue
       error ->
         handle_error(error, __STACKTRACE__)
@@ -26,9 +34,8 @@ defmodule Bonfire.Mailer do
 
   def send_app_feedback(type \\ "feedback", subject, body) do
     config = Config.get(__MODULE__, [])
-    # Keyword.get(config, :feedback_from, "team@bonfire.cafe")
-    from = Keyword.get(config, :reply_to, "noreply@bonfire.local")
-    to = Keyword.get(config, :feedback_to, "bonfire@fire.fundersclub.com")
+    from = Keyword.get(config, :reply_to, @default_email)
+    to = Keyword.get(config, :feedback_to, @team_email)
 
     app_name = Bonfire.Application.name()
 
@@ -40,13 +47,13 @@ defmodule Bonfire.Mailer do
   end
 
   def handle_error(error, stacktrace \\ nil) do
-    e = Map.get(error, :raw, error)
+    e = Utils.e(error, :raw, nil) || error
     error(stacktrace, "Email delivery error: #{inspect(e)}")
 
     case e do
       {:no_credentials, _} -> {:error, :mailer_config}
       {:retries_exceeded, _} -> {:error, :mailer_retries_exceeded}
-      %Bamboo.ApiError{message: msg} -> {:error, :mailer_api_error}
+      %Bamboo.ApiError{message: _msg} -> {:error, :mailer_api_error}
       # give up
       _ -> {:error, :mailer}
     end
